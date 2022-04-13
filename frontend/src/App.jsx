e52f71179cbde79646dc4a322e82902e23e09750
Alexis Olivier <alexis-olivier_student2022@wilder.school>
2022-04-13 15:50:23 +0200

    maj erreurs mineures corrigées

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import Home from "@pages/Home";
-
+/* eslint-disable import/no-unresolved */
+import Bouton from "@components/Bouton";
 import "./App.css";
+import prairie from "@assets/prairieChampignons.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${prairie})`,
+        height: "800px",
+        backgroundSize: "cover",
+      }}
+    >
+      <Bouton name="Règles" className="petitBouton" />
+      <Bouton name="JOUER" className="grosBouton" />
+      <Bouton name="Contact" className="petitBouton" />
     </div>
   );
 }
