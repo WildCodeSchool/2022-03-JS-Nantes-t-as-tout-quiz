9793cb267acdcbde076795e58e21b45cf5ee3f7e
MariePerraud <m100chez@gmail.com>
2022-04-14 16:03:35 +0200

    us1: maj syntax home

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logoToto from "@assets/logo_toto.png";
+import "../App.css";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">
+          <img
+            src={logoToto}
+            alt="logo d'un tatou, mascotte de l'application"
+          />
+        </NavLink>
+        <h1>T&apos;As Tout Quiz</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
