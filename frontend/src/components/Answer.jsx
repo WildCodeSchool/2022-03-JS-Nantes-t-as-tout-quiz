c1d2bc4034262d50df47c3ee7b9161924ff61baf
JeromeVigneron <jerome.vigneron@net-c.fr>
2022-04-21 16:11:11 +0200

    US4 - Creation of the basic display of the game page

--- frontend/src/components/Answer.jsx
+++ frontend/src/components/Answer.jsx
@@ -1,1 +1,13 @@
-import "./Answer.css";
+import Bouton from "@components/Bouton";
+import "@components/Answer.css";
+
+export default function Answer() {
+  return (
+    <section>
+      <Bouton name="Celestia" className="button answ-one" />
+      <Bouton name="Azeroth" className="button answ-two" />
+      <Bouton name="Tyrie" className="button answ-three" />
+      <Bouton name="Koprolu" className="button answ-four" />
+    </section>
+  );
+}
