b3740088d12672e0a1ddf51866d5b018bb07f332
Alexis Olivier <alexis-olivier_student2022@wilder.school>
2022-04-22 16:48:04 +0200

    lien vers page game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import Header from "@components/Header";
 import Home from "@pages/Home";
 import Regles from "@pages/Regles";
+import Game from "@pages/Game";
 import Contact from "@pages/Contact";
 import NotificationMessage from "@components/NotificationMessage";
 import { Route, Routes } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Regles" element={<Regles />} />
+        <Route path="/Game" element={<Game />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/NotificationMessage" element={<NotificationMessage />} />
       </Routes>
