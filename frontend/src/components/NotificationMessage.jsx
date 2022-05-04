16c68c9f0d195dcccfda87131c7ea588a6a5674f
JeromeVigneron <jerome.vigneron@net-c.fr>
2022-05-04 15:13:18 +0200

    US9 - Modification of the App.jsx, Contact.jsx and NotificationMessage.jsx structure and linked CSS

--- frontend/src/components/NotificationMessage.jsx
+++ frontend/src/components/NotificationMessage.jsx
@@ -7,26 +7,28 @@ import { NavLink } from "react-router-dom";
 
 function NotificationMessage() {
   return (
-    <div
+    <main
       className="App"
       style={{
         backgroundImage: `url(${prairie})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        minHeight: "100vh",
+        height: "1200px",
       }}
     >
-      <h2 className="notifMsg">Message envoyé</h2>
-      <div className="fondIcone">
-        <NavLink to="/">
-          <img
-            className="logoHome"
-            src={logoHome}
-            alt="logo d'une maison: permet le retour à la page d'accueil"
-          />
-        </NavLink>
-      </div>
-    </div>
+      <section>
+        <h2 className="notifMsg">Message envoyé</h2>
+        <div className="fondIcone">
+          <NavLink className="navLink" to="/">
+            <img
+              className="logoHome"
+              src={logoHome}
+              alt="logo d'une maison: permet le retour à la page d'accueil"
+            />
+          </NavLink>
+        </div>
+      </section>
+    </main>
   );
 }
 
